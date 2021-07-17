255f56633f36234d526b97a135283fe5fbe842f9
BXBots <86224222+BXBots@users.noreply.github.com>
2021-07-17 11:34:24 +0530

    Update incoming_message_fn.py

--- bot/plugins/incoming_message_fn.py
+++ bot/plugins/incoming_message_fn.py
@@ -100,7 +100,7 @@ async def incoming_start_message_f(bot, update):
                 ],
                 [
                     InlineKeyboardButton('⏳ Status ⏳', url='https://t.me/BXCompressStatus'),
-                    InlineKeyboardButton('⚙️Help, callback_data=("help")
+                    InlineKeyboardButton('👲 Developer, url=('https://t.me/Mufaz123')
                 ]
             ]
         ),
